5f89bc5b326e2fe891d8cd79a31dd5c7c633f6c9
moretzu <72914371+moretzu@users.noreply.github.com>
2021-02-02 14:28:54 +0400

    introduced usign namespace

--- absolute_basics/print_something.cpp
+++ absolute_basics/print_something.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+// helps accessing namespace's objects easier
+using namespace std;
+
 int main()
 {
-    std::cout << "well this is how you print things" << std::endl;
+    // here we no longer need `std` because of the statement above
+    cout << "well this is how you print things" << endl;
+    cout << "i am モーレツ (moretsu) btw" << endl;
 }
